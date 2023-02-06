921d49de63260d7f4b94bba1eb1c7b7c082f5249
RajaGopikaSri <rajagopikasrimuthukumar5@gmail.com>
2023-02-06 11:39:49 +0530

    nchange

--- positive.java
+++ positive.java
@@ -1,22 +1,24 @@
-<<<<<<< HEAD
+
 public class positive
 {  
- 
 public static void main(String[] args)   
 {  
 //number to be check  
 int num=912;  
 //checks the number is greater than 0 or not  
-if(num=>0)  
+if(num>0)  
 {  
 System.out.println("The number is positive.");  
 }  
 //checks the number is less than 0 or not  
-else(num<0)  
+else if(num<0)  
 {  
 System.out.println("The number is negative.");  
 }  
 //executes when the above two conditions return false  
-
+else  
+{  
+System.out.println("The number is zero.");  
+}  
 }  
 }  
